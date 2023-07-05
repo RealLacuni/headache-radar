d0aca10b1ee0410abd431e2348d92f6944da00a3
RealLacuni <71047915+RealLacuni@users.noreply.github.com>
2023-07-04 20:57:09 -0400

    Began routing setup and overall minor changes. pages go in /routes, app -> root.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,25 @@
-import ReactDOM from 'react-dom/client'
-import App from './App';
+import * as ReactDOM from "react-dom/client";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import Root from "./routes/Root.tsx";
+import ErrorPage from "./components/ErrorPage.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root/>,
+        errorElement: <ErrorPage/>,
+        children: [
+            {
+                path: "child/",
+                element: <h1>Home</h1>,
+            }
+        ]
+    }
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />
+    <RouterProvider router={router}/>
 )
